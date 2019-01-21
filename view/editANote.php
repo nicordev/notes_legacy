44695d2a5da2a79247ead501bd2c1f3819148a7a
SansGodasse <sansgodasse@gmail.com>
2019-01-21 21:51:09 +0100

    Edit note content

--- view/editANote.php
+++ view/editANote.php
@@ -1,12 +1,18 @@
 <!-- Edit the note -->
 <div>
     <form action="index.php" method="post">
-        <input class="note-edit" type="text" name="n_title" <?= 'value="' . $note->getTitle() . '"' ?>>
+        <p>
+            <input class="note-edit-title" type="text" name="n_title" <?= 'value="' . $note->getTitle() . '"' ?>>
+            <input class="note-btn" type="submit" value="🗸">
+        </p>
+        <p>
+            <textarea class="note-edit-content" name="n_content" cols="30" rows="10"></textarea>
+        </p>
         <input type="hidden" name="save_modifications">
         <input type="hidden" name="n_id" <?= 'value="' . $note->getId() . '"' ?>>
-        <input class="note-btn" type="submit" value="🗸">
     </form>
 </div>
+
 <div class="note-commands">
     <!-- Cancel -->
     <form action="index.php" method="post">
